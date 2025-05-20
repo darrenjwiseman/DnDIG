bdc8db92a26a0ad70da20383070a6fe57ae8343b
darrenjwiseman <darrenjwiseman@gmail.com>
2025-05-19 20:50:39 -0700

    updated weapon tables for 5e 2024, including mastery & new weapons

--- DnDIG.py
+++ DnDIG.py
@@ -10,55 +10,55 @@ class Item:
 	cost: str
 	damage: str
 	weight: int
-	properties: str = ''
 	mastery: str = ''
+	properties: str = ''
 
 categories = {
 	'Simple Melee weapon   ': [
-		Item("Club", "1 sp.", "1d4 bludgeon", 2, "Light", "Slow"),
-		Item("Dagger", "2 gp.", "1d4 pierce", 1, "Finesse, Light, Thrown (range 20/60)", "Nick"),
-		Item("Greatclub", "2 sp.", "1d8 bludgeoning", 10, "Two-handed", "Push"),
-		Item("Handaxe", "5 gp.", "1d6 slashing", 2, "Light, Thrown (range 20/60)", "Vex"),
-		Item("Javelin", "5 sp.", "1d6 piercing", 2, "Thrown (range 30/120)", "Slow"),
-		Item("Hammer, Light", "2 gp.", "1d bludgeoning", 2, "Light, Thrown (range 20/60)", "Nick"),
-		Item("Mace", "5 gp.", "1d6 bludgeoning", 4, "none", "Sap"),
-		Item("Quarterstaff", "2 sp.", "1d6 bludgeoning", 4, "Versatile (1d8)", "Topple"),
-		Item("Sickle", "1 gp.", "1d4 slashing", 2, "Light", "Nick"),
-		Item("Spear", "1 gp.", "1d6 piercing", 3, "Thrown (range 20/60), Versatile (1d8)", "Sap")
-		# ... other items in SimpleMelee category
+		Item("Club", "1 sp.", "1d4 bludgeon", 2, "Slow", "Light"),
+		Item("Dagger", "2 gp.", "1d4 pierce", 1, "Nick", "Finesse, Light, Thrown (range 20/60)"),
+		Item("Greatclub", "2 sp.", "1d8 bludgeoning", 10, "Push", "Two-handed"),
+		Item("Handaxe", "5 gp.", "1d6 slashing", 2, "Vex", "Light, Thrown (range 20/60)"),
+		Item("Javelin", "5 sp.", "1d6 piercing", 2, "Slow", "Thrown (range 30/120)"),
+		Item("Hammer, Light", "2 gp.", "1d bludgeoning", 2, "Nick", "Light, Thrown (range 20/60)"),
+		Item("Mace", "5 gp.", "1d6 bludgeoning", 4, "Sap", "none"),
+		Item("Quarterstaff", "2 sp.", "1d6 bludgeoning", 4, "Topple", "Versatile (1d8)"),
+		Item("Sickle", "1 gp.", "1d4 slashing", 2, "Nick", "Light"),
+		Item("Spear", "1 gp.", "1d6 piercing", 3, "Sap", "Thrown (range 20/60), Versatile (1d8)")
 	],
 	'Simple Ranged weapon  ': [
-        Item("Crossbow, Light", "25 gp.", "1d8 piercing", 5, "Ammunition (range 80/320), Loading, Two-handed", "Slow"),
-		Item("Dart", "5 cp.", "1d4 piercing", 0.25, "Finesse, Thrown (range 20/60)", "Vex"),
-		Item("Shortbow", "25 gp.", "1d6 piercing", 2, "Ammunition (range 80/320), Two-handed", "Vex"),
-		Item("Sling", "1 sp.", "1d4 bludgeoning", 0, "Ammunition (range 30/120)", "Slow")
-		# ... other items in Simpleranged category
+        Item("Crossbow, Light", "25 gp.", "1d8 piercing", 5, "Slow", "Ammunition (range 80/320), Loading, Two-handed"),
+		Item("Dart", "5 cp.", "1d4 piercing", 0.25, "Vex", "Finesse, Thrown (range 20/60)"),
+		Item("Shortbow", "25 gp.", "1d6 piercing", 2, "Vex", "Ammunition (range 80/320), Two-handed"),
+		Item("Sling", "1 sp.", "1d4 bludgeoning", 0, "Slow", "Ammunition (range 30/120)")
 	],
 	'Martial Melee weapon  ': [
-		Item("Battleaxe", "10 gp", "1d8 slashing", 4, "Versatile (1d10)"),
-		Item("Flail", "10 gp", "1d8 bludgeoning", 2, ),
-		Item("Glaive", "20 gp", "1d10 slashing", 6, "Heavy, Reach, Two-handed"),
-		Item("Greataxe", "30 gp", "1d12 slashing", 7, "Heavy, Two-handed"),
-		Item("Greatsword", "50 gp", "2d6 slashing", 6, "Heavy, Two-handed"),
-		Item("Halberd", "20 gp", "1d10 slashing", 6, "Heavy, Reach, Two-handed"),
-		Item("Lance", "10 gp", "1d12 piercing", 6, "Reach, special"),
-		Item("Longsword", "15 gp", "1d8 slashing", 3, "Versatile (1d10)"),
-		Item("Morningstar", "15 gp", "1d8 piercing", 4, "none"),
-		Item("Pike", "5 gp", "1d10 piercing", 18, "Heavy, Reach, Two-handed"),
-		Item("Rapier", "25 gp", "1d8 piercing", 2, "Finesse"),
-		Item("Scimitar", "25 gp", "1d6 slashing", 3, "Finesse, Light"),
-		Item("Shortsword", "10 gp", "1d6 piercing", 2, "Finesse, Light"),
-		Item("Trident", "5 gp", "1d6 piercing", 4, "Thrown (range 20/60), Versatile (1d8)"),
-		Item("War pick", "5 gp", "1d8 piercing", 2, "none"),
-		Item("War hammer", "15 gp", "1d8 bludgeoning", 2, "none")
-		# ... other items in MartialMelee category
+        Item("Battleaxe", "10 gp", "1d8 slashing", 4, "Topple", "Versatile (1d10)"),
+        Item("Flail", "10 gp", "1d8 bludgeoning", 2, "Sap", "none"),
+        Item("Glaive", "20 gp", "1d10 slashing", 6, "Graze", "Heavy, Reach, Two-handed"),
+        Item("Greataxe", "30 gp", "1d12 slashing", 7, "Cleave", "Heavy, Two-handed"),
+        Item("Greatsword", "50 gp", "2d6 slashing", 6, "Graze", "Heavy, Two-handed"),
+        Item("Halberd", "20 gp", "1d10 slashing", 6, "Cleave", "Heavy, Reach, Two-handed"),
+        Item("Lance", "10 gp", "1d12 piercing", 6, "Topple", "Reach, special"),
+        Item("Longsword", "15 gp", "1d8 slashing", 3, "Sap", "Versatile (1d10)"),
+        Item("Maul", "10 gp", "2d6 bludgeoning", 10, "Topple", "Heavy, Two-Handed"),
+        Item("Morningstar", "15 gp", "1d8 piercing", 4, "Sap", "none"),
+        Item("Pike", "5 gp", "1d10 piercing", 18, "Push", "Heavy, Reach, Two-handed"),
+        Item("Rapier", "25 gp", "1d8 piercing", 2, "Vex", "Finesse"),
+        Item("Scimitar", "25 gp", "1d6 slashing", 3, "Nick", "Finesse, Light"),
+        Item("Shortsword", "10 gp", "1d6 piercing", 2, "Vex", "Finesse, Light"),
+        Item("Trident", "5 gp", "1d6 piercing", 4, "Topple", "Thrown (range 20/60), Versatile (1d8)"),
+        Item("War hammer", "15 gp", "1d8 bludgeoning", 2, "Push", "none"),
+        Item("War pick", "5 gp", "1d8 piercing", 2, "Sap", "none"),
+        Item("Whip", "5 gp", "1d4 slashing", 2, "Slow", "Finesse, Reach"),
 	],
 	'Martial Ranged weapon ': [
-		Item("Blowgun", "10 gp.", "1 piercing", 1, "Ammunition (range 25/100), Loading"),
-		Item("Crossbow, hand", "75 gp.", "1d6 piercing", 3, "Ammunition (range 30/120), Light, Loading"),
-		Item("Crossbow, heavy", "50 gp.", "1d10 piercing", 18, "Ammunition (range 100/400), Heavy, Loading, Two-handed"),
-		Item("Longbow", "50 gp.", "1d8 piercing", 2, "Ammunition (range 150/600), Heavy, Two-handed")
-		# ... other items in MartrialRanged category
+		Item("Blowgun", "10 gp.", "1 piercing", 1, "Vex", "Ammunition (range 25/100), Loading"),
+		Item("Crossbow, hand", "75 gp.", "1d6 piercing", 3, "Vex", "Ammunition (range 30/120), Light, Loading"),
+		Item("Crossbow, heavy", "50 gp.", "1d10 piercing", 18, "Push","Ammunition (range 100/400), Heavy, Loading, Two-handed"),
+		Item("Longbow", "50 gp.", "1d8 piercing", 2, "Slow","Ammunition (range 150/600), Heavy, Two-handed"),
+		Item("Musket", "500 gp.", "1d12 piercing", 10, "Slow","Ammunition (Range 40/120), Loading, Two-Handed"),
+		Item("Pistol", "250 gp.", "1d10 piercing", 3, "Vex","Ammunition (range 30/90), Loading")
 	]
 }
 
